82da3e0271c911c4668ac1a51a32699ab674b3d2
Anney Park <anneypark@gmail.com>
2020-07-15 01:05:24 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -2,6 +2,10 @@ if TRUE
   puts "This code runs!"
 end
 
+if FALSE
+  puts "This code does not run. You will not see this in IRB"
+end
+
 run_code_inside = TRUE
 puts "Code before if...end"
 if run_code_inside
