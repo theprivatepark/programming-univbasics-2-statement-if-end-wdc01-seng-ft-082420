b682728033f9fbe506ab3bd339f01b1aee8f4286
Anney Park <anneypark@gmail.com>
2020-07-15 01:32:11 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,14 +1,8 @@
-if TRUE
-  puts "This code runs!"
+chance_of_rain = 0.2 
+puts "Let's go outside!"
+if chance_of_rain > 0.5
+  puts "Pack an umbrella!"
+else
+  puts "Enjoy the fine day!"
 end
-
-if FALSE
-  puts "This code does not run. You will not see this in IRB"
-end
-
-run_code_inside = TRUE
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
-end
-puts "Code after if...end"
+puts "Oh, and always wear sunscreen!"
